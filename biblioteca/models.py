8ca79aaeff29d709d9195795f469349d39bef4b9
ESTUDIANTES <ESTUDIANTES@45PC01LAB3.inatec.edu.ni>
2025-07-01 11:17:53 -0600

    first commit

---
+++ biblioteca/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    fecha_publicacion = models.DateField()
+
+    def __str__(self):
+        return self.titulo
